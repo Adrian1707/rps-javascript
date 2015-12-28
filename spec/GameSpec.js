afbe634da78174802bd2718d77e2b002abbecb8c
Adrian Booth <Adrian.booth17@gmail.com>
2015-12-28 12:49:58 +0000

    First test passing. Choice should equal argument passed into userSelect function

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,3 +1,17 @@
 describe("Game", function() {
-  
+
+  var game;
+
+  beforeEach(function(){
+    game = new Game();
+  });
+
+  describe('user choice', function(){
+
+    it('should equal the choice the user selected',function(){
+      game.userSelect("Rock");
+      expect(game.userChoice).toEqual("Rock")
+    });
+  })
+
 });
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,8 @@
 function Game() {
+  this.choices = ["rock","paper","scissors"];
+  this.userChoice = "";
+}
 
+Game.prototype.userSelect = function(choice){
+  this.userChoice = choice;
 }
