f6aba22e10f8bc8e436c80f047d8597d67ceb84f
Adrian Booth <Adrian.booth17@gmail.com>
2015-12-28 16:01:03 +0000

    spyOn not working as expected, but test passing for draw function after setting opponentChoice equal to Rock in test

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,9 +8,26 @@ describe("Game", function() {
 
   describe('user choice', function(){
 
-    it('should equal the choice the user selected',function(){
+    it('should equal the choice the user selected', function(){
       game.userSelect("Rock");
       expect(game.userChoice).toEqual("Rock")
+    });
+  })
+
+  describe('draw', function(){
+
+    // beforeEach(function(){
+    //   spyOn(game,"compChoice").and.returnValue("Rock");
+    // })
+
+    it('should equal true if user choice and comp choice are the same', function(){
+      game.userSelect("Rock");
+      game.compChoice("Rock");
+      game.opponentChoice = "Rock";
+      // spyOn(game,'compChoice').and.returnValue("Rock");
+
+      // expect(game.opponentChoice).toEqual("Rock")
+      expect(game.draw()).toEqual(true);
     });
   })
 
--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,38 @@
 function Game() {
-  this.choices = ["rock","paper","scissors"];
+  this.choices = ["Rock","Paper","Scissors"];
   this.userChoice = "";
+  this.opponentChoice = "";
 }
 
 Game.prototype.userSelect = function(choice){
   this.userChoice = choice;
 }
+
+Game.prototype.compChoice = function(){
+  this.opponentChoice = this.choices[Math.floor(Math.random()*this.choices.length)];
+  return this.opponentChoice;
+}
+
+Game.prototype.draw = function(){
+  if(this.userChoice === this.opponentChoice){
+    return true;
+  }
+  else{
+    return false;
+  };
+}
+
+var game = new Game();
+
+game.userSelect("Rock")
+
+game.compChoice();
+
+// debug(game.draw());
+
+
+// var items = ["rock","paper","scissors"]
+
+// var random = items[Math.floor(Math.random()*items.length)]
+
+// debug(random);
