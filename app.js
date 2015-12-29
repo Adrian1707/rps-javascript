b68f7c470386478eb4a890d92a64a115d230c356
Adrian Booth <Adrian.booth17@gmail.com>
2015-12-29 16:03:22 +0000

    Form set up to submit name. Renders weapon page on submission

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+app.set('views', './views');
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
   res.render('index');
+});
+
+app.get('/weapon', function (req, res){
+	res.render('weapon');
+});
+
+app.post('/weapon',function (req,res){
+	res.redirect('/weapon');
 });
 
 var server = app.listen(3000, function () {
